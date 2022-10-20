8abf2b7e3931457d5912fd3b47fa23de0052ede1
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-10-20 09:50:49 +0100

    fix dialect

--- models/index.js
+++ models/index.js
@@ -6,14 +6,14 @@ const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+  dialect: "mysql",
   operatorsAliases: false,
 
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
