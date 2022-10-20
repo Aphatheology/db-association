531474c3d35d181931c3bccb50485bc631b61b3a
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-10-20 11:16:04 +0100

    connect with uri

--- models/index.js
+++ models/index.js
@@ -1,21 +1,9 @@
 require('dotenv').config()
 
 const dbConfig = require("../config/db.config.js");
-
-
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: "mysql",
-  operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
+const sequelize = new Sequelize(`${process.env.DB_URI}`)
 
 const db = {};
 
